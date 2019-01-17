fef076be8cbd43ab418366d5796d8b953c4f311f
GaneshSAcharya <31121986g@gmail.com>
2019-01-17 15:43:33 +0530

    Loops

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import NameComponent from './components/NameComponent'
-import './App.css';
+//import NameComponent from './components/NameComponent'
+
 
 class App extends Component {
+
   render() {
 
-    var divStyle = {
-      fontFamily : 'Calibri',
-      fontSize : '10em',
-      fontStyle: 'italic',
-      color : 'red',
-      border: '2px solid lime',
-      margin: '100px',
-      padding: '0px'
-    };
+    // var divStyle = {
+    //   fontFamily : 'Calibri',
+    //   fontSize : '10em',
+    //   fontStyle: 'italic',
+    //   color : 'red',
+    //   border: '2px solid lime',
+    //   margin: '100px',
+    //   padding: '0px'
+    // };
+
+    var welcomeMessage = ["Welcome", "to", "my", "new", "world!"];
+    var toShow = [];
     
+    welcomeMessage.forEach((word) => {
+
+      toShow.push(
+        <p>{word}</p>
+      );
+
+    });
 
     return (
-      <div className="App" style = {divStyle}>
-         <NameComponent />
+      <div >
+        {toShow}
       </div>
    
     );
+
+
   }
 }
 
